a23b3e50adfed76d9a995e1757d566694e8e5940
AlanSalgado <s20120202@alumnos.itsur.edu.mx>
2023-09-23 22:55:14 -0600

    Adición de clase, objeto, archivos drawables y strings

---
+++ app/src/main/java/com/alansalgadosalas/app_superheroes/Model/Hero.kt
@@ -0,0 +1,13 @@
+package com.alansalgadosalas.app_superheroes.Model
+
+class Hero {
+    var nombre: Int
+    var descripcion: Int
+    var imagen: Int
+
+    constructor(nombre: Int, descripcion: Int, imagen: Int){
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.imagen = imagen
+    }
+}
