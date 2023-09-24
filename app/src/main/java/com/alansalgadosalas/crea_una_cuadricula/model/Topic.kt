94285b487bfb7601e95ca48e33b40fb124357dbf
AlanSalgado <s20120202@alumnos.itsur.edu.mx>
2023-09-24 01:10:41 -0600

    Configuración de la fuente de datos (Adición de elementos strings y del objeto DataSource)

---
+++ app/src/main/java/com/alansalgadosalas/crea_una_cuadricula/model/Topic.kt
@@ -0,0 +1,13 @@
+package com.alansalgadosalas.crea_una_cuadricula.model
+
+class Topic {
+    var nombre: Int
+    var cantidad: Int
+    var imagen: Int
+
+    constructor(nombre: Int, cantidad: Int, imagen: Int){
+        this.nombre = nombre
+        this.cantidad = cantidad
+        this.imagen = imagen
+    }
+}
